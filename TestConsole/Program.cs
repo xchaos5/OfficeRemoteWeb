f2a9a134cbdad3e4231b3b884f3c078ab25a0af1
xchaos5 <xchaos@163.com>
2014-01-19 12:38:26 +0800

    Move IIS Express to stand alone Console

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -5,6 +5,10 @@ using System.Text;
 using System.Threading.Tasks;
 using PPt = Microsoft.Office.Interop.PowerPoint;
 using System.Runtime.InteropServices;
+using System.Threading;
+using System.Net;
+using System.IO;
+using System.Diagnostics;
 
 namespace TestConsole
 {
@@ -17,37 +21,78 @@ namespace TestConsole
         static int slidescount;
         static int slideIndex;
 
+        static string GetContentType(string path)
+        {
+            string ext = "";
+            int dotIndex = path.LastIndexOf('.');
+            if (dotIndex >= 0)
+                ext = path.Substring(dotIndex + 1);
+            
+            string contentType;
+            switch (ext)
+            {
+                case "htm":
+                    contentType = "text/html";
+                    break;
+                case "html":
+                    contentType = "text/html";
+                    break;
+                case "js":
+                    contentType = "text/javascript";
+                    break;
+                case "css":
+                    contentType = "text/css";
+                    break;
+                case "png":
+                    contentType = "image/png";
+                    break;
+                case "jpg":
+                    contentType = "image/jpeg";
+                    break;
+                case "jpeg":
+                    contentType = "image/jpeg";
+                    break;
+                default:
+                    contentType = "text/plain";
+                    break;
+            }
+            return contentType;
+        }
+
         static void Main(string[] args)
         {
-            try
+            var thread = new Thread(new ThreadStart(() =>
             {
-                // Get Running PowerPoint Application object 
-                pptApplication = System.Runtime.InteropServices.Marshal.GetActiveObject("PowerPoint.Application") as PPt.Application;
-            }
-            catch
-            {
-
-            }
-            if (pptApplication != null)
-            {
-                // Get Presentation Object 
-                presentation = pptApplication.ActivePresentation;
-                // Get Slide collection object 
-                slides = presentation.Slides;
-                // Get Slide count 
-                slidescount = slides.Count;
-                // Get current selected slide  
+                byte[] sendData = null;
+                string dir = Environment.CurrentDirectory;  //Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName);
                 try
                 {
-                    // Get selected slide object in normal view 
-                    slide = slides[pptApplication.ActiveWindow.Selection.SlideRange.SlideNumber];
+                    HttpListener listener = new HttpListener();
+                    listener.Prefixes.Add("http://+:8080/");
+                    listener.Start();
+
+                    while (true)
+                    {
+                        var context = listener.GetContext();
+                        Console.WriteLine(string.Format("{0} {1}", context.Request.HttpMethod, context.Request.Url.AbsolutePath));
+                        if (File.Exists(dir + context.Request.Url.LocalPath))
+                            sendData = File.ReadAllBytes(dir + context.Request.Url.LocalPath);
+                        else
+                            sendData = System.Text.Encoding.Default.GetBytes("<b>404 File not found<b>....");
+                        context.Response.ContentType = GetContentType(context.Request.Url.LocalPath);
+                        context.Response.Close(sendData, true);
+                    }
                 }
-                catch
+                catch (Exception ex)
                 {
-                    // Get selected slide object in reading view 
-                    slide = pptApplication.SlideShowWindows[1].View.Slide;
+                    Console.WriteLine(ex.ToString());
                 }
-            }
+            }));
+            thread.IsBackground = true;
+            thread.Start();
+
+            Console.WriteLine("Server started.");
+            Console.ReadLine();
         }
     }
 }
